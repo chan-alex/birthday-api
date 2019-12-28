e27034beb1c8ecae980a7bb83f7e4a741d699ce3
alex chan <chan.k.leong@gmail.com>
2019-12-28 15:45:06 +0800

    Introducing sqlalchemy to start saving data

--- code/Models.py
+++ code/Models.py
@@ -1,14 +1,29 @@
 import datetime
+from db import db
 
 
-class BirthdayModel:
-    def __init__(self, name, dob):
-        self.name = name
-        self.dob = dob
-    
+class BirthdayModel(db.Model):
+    __tablename__ = 'birthdays'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    dateOfBirth = db.Column(db.String(10))
+
+
+    def __init__(self, username, dob):
+        self.username = username
+        self.dateOfBirth = dob
+
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+
     @classmethod
     def valid_username(cls, name):
         return name.isalpha() == True
+
 
     @classmethod
     def valid_dob(cls, dob):
@@ -19,3 +34,7 @@ class BirthdayModel:
             return False
         return True
 
+
+    @classmethod
+    def find_by_username(cls, name):
+        return cls.query.filter_by(username=name).first()
--- code/app.py
+++ code/app.py
@@ -5,17 +5,27 @@ from Models import BirthdayModel
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 class Birthday(Resource):
 
     def get(self, username):
+        if BirthdayModel.valid_username(username) == False:
+            return { "message": "User name should contain alphabets only" }, 422  # return for easier testing.
+
+        print(BirthdayModel.find_by_username(username))
         return { "message": "Hello, {}. Your birthday is in N days".format(username) }
 
 
     def put(self, username):
-
         if BirthdayModel.valid_username(username) == False:
             return { "message": "User name should contain alphabets only" }, 422  # return for easier testing.
 
@@ -30,9 +40,16 @@ class Birthday(Resource):
         if BirthdayModel.valid_dob(dob) == False:
             return { "message": "dateOfBirth should be in YYYY-MM-DD" }, 400  # return for easier testing.
 
+        birthday = BirthdayModel(username,dob)
+        birthday.save_to_db()
+
         return "", 204
 
 
 api.add_resource(Birthday, '/hello/<string:username>')
 
-app.run(port=5000, debug=True)    
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)    
